806affb7d09e1626cc868b4279cf1c5e6631aa97
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-01 10:08:17 +0200

    02 vygenerovana trieda Datum cez UML .FRI plugin Simple Java Generator

---
+++ 02/datum/Datum.java
@@ -0,0 +1,25 @@
+public class Datum {
+    private int den;
+    private int mesiac;
+    private int rok;
+    
+    public Datum(int den, int mesiac, int rok) {
+        
+    }
+    
+    public int getDen() {
+        
+    }
+    
+    public int getMesiac() {
+        
+    }
+    
+    public int getRok() {
+        
+    }
+    
+    public String getDatum() {
+        
+    }
+}
