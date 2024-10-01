7d6e54143ca33541181877b41143018735851581
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-01 10:20:00 +0200

    02 implementacia triedy Datum

--- 02/datum/Datum.java
+++ 02/datum/Datum.java
@@ -4,22 +4,24 @@ public class Datum {
     private int rok;
     
     public Datum(int den, int mesiac, int rok) {
-        
+        this.den = den;
+        this.mesiac = mesiac;
+        this.rok = rok;
     }
     
     public int getDen() {
-        
+        return this.den;
     }
     
     public int getMesiac() {
-        
+        return this.mesiac;
     }
     
     public int getRok() {
-        
+        return this.rok;
     }
     
     public String getDatum() {
-        
+        return this.den + ". " + this.mesiac + ". " + this.rok;
     }
 }
