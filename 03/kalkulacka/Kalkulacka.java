84d7a41caec3a8275f8c374a7ab01be43082575e
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-08 08:53:59 +0200

    03 kalkulacka prepisana z UML

---
+++ 03/kalkulacka/Kalkulacka.java
@@ -0,0 +1,30 @@
+public class Kalkulacka {
+    
+    public Kalkulacka() {
+        
+    }
+    
+    public void zadaj(double cislo) {
+        
+    }
+    
+    public void pricitaj(double cislo) {
+        
+    }
+    
+    public void odcitaj(double cislo) {
+        
+    }
+    
+    public void vynasob(double cislo) {
+        
+    }
+    
+    public void vydel(double cislo) {
+        
+    }
+    
+    public double getVysledok() {
+        return 0;
+    }
+}
