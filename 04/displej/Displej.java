f08f96e45443689318391b4c6e58ff6110029b0e
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-15 10:18:09 +0200

    04 zobrazenie cislice na displeji

--- 04/displej/Displej.java
+++ 04/displej/Displej.java
@@ -22,10 +22,101 @@ public class Displej {
         this.segmentD = new Segment(5, 110, false);
         this.segmentE = new Segment(0, 60, true);
         this.segmentF = new Segment(0, 5, true);
-        this.segmentD = new Segment(5, 55, false);
+        this.segmentG = new Segment(5, 55, false);
     }
     
     public void zmenCislicu(int cislica) {
-        
+        switch (cislica) {
+            case 0:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.zapni();
+                this.segmentF.zapni();
+                this.segmentG.vypni();
+                break;
+            case 1:
+                this.segmentA.vypni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.vypni();
+                this.segmentE.vypni();
+                this.segmentF.vypni();
+                this.segmentG.vypni();
+                break;
+            case 2:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.vypni();
+                this.segmentD.zapni();
+                this.segmentE.zapni();
+                this.segmentF.vypni();
+                this.segmentG.zapni();
+                break;
+            case 3:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.vypni();
+                this.segmentF.vypni();
+                this.segmentG.zapni();
+                break;
+            case 4:
+                this.segmentA.vypni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.vypni();
+                this.segmentE.vypni();
+                this.segmentF.zapni();
+                this.segmentG.zapni();
+                break;
+            case 5:
+                this.segmentA.zapni();
+                this.segmentB.vypni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.vypni();
+                this.segmentF.zapni();
+                this.segmentG.zapni();
+                break;
+            case 6:
+                this.segmentA.zapni();
+                this.segmentB.vypni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.zapni();
+                this.segmentF.zapni();
+                this.segmentG.zapni();
+                break;
+            case 7:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.vypni();
+                this.segmentE.vypni();
+                this.segmentF.vypni();
+                this.segmentG.vypni();
+                break;
+            case 8:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.zapni();
+                this.segmentF.zapni();
+                this.segmentG.zapni();
+                break;
+            case 9:
+                this.segmentA.zapni();
+                this.segmentB.zapni();
+                this.segmentC.zapni();
+                this.segmentD.zapni();
+                this.segmentE.vypni();
+                this.segmentF.zapni();
+                this.segmentG.zapni();
+                break;
+        }
     }
 }
