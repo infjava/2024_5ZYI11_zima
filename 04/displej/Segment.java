4f252788ccc4b66ff15cb5005911f2a409727343
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-15 09:30:44 +0200

    implementacia konstruktora triedy Segment

--- 04/displej/Segment.java
+++ 04/displej/Segment.java
@@ -3,6 +3,17 @@ public class Segment {
     
     public Segment(int x, int y, boolean jeZvislo) {
         this.segment = new Obdlznik();
+        this.segment.zmenFarbu("red");
+        this.segment.posunVodorovne(x - 60);
+        this.segment.posunZvisle(y - 50);
+        
+        if (jeZvislo == true) {
+            this.segment.zmenStrany(5, 50);
+        } else {
+            this.segment.zmenStrany(50, 5);
+        }
+        
+        this.segment.zobraz();
     }
     
     public void zapni() {
