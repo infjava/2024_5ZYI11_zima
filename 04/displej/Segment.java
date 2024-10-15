42ce21c363b641585b315cd2044f20d62bb38142
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-15 09:19:26 +0200

    04 trieda Segment exportovana z UML do BlueJ projektu

---
+++ 04/displej/Segment.java
@@ -0,0 +1,15 @@
+public class Segment {
+    private Obdlznik segment;
+    
+    public Segment(int x, int y, boolean jeZvislo) {
+        this.segment = new Obdlznik();
+    }
+    
+    public void zapni() {
+        
+    }
+    
+    public void vypni() {
+        
+    }
+}
