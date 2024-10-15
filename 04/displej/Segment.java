9a3b52416ba1b9bafcd2880e83b34e0a3cf1b8e9
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-15 09:31:12 +0200

    04 opravenie booleanovej podmienky

--- 04/displej/Segment.java
+++ 04/displej/Segment.java
@@ -7,7 +7,7 @@ public class Segment {
         this.segment.posunVodorovne(x - 60);
         this.segment.posunZvisle(y - 50);
         
-        if (jeZvislo == true) {
+        if (jeZvislo) {
             this.segment.zmenStrany(5, 50);
         } else {
             this.segment.zmenStrany(50, 5);
