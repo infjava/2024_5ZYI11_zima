8c5f28bbe589349435643775212447937dd24387
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-15 09:54:11 +0200

    04 displej z UML

---
+++ 04/displej/Displej.java
@@ -0,0 +1,12 @@
+public class Displej {
+    private Obdlznik pozadie;
+    private Segment segmentA;
+    
+    public Displej() {
+        
+    }
+    
+    public void zmenCislicu(int cislica) {
+        
+    }
+}
