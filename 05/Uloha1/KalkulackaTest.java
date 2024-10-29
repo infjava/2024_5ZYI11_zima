56823298d6ec5c1f25101105e21ddd9c2aeb01b3
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-29 09:02:08 +0100

    05 vytvorena testova trieda

---
+++ 05/Uloha1/KalkulackaTest.java
@@ -0,0 +1,24 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class KalkulackaTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class KalkulackaTest {
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp() {
+        
+    }
+}
+
