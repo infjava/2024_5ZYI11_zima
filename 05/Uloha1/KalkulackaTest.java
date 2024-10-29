76b6dde9704ae3caba19c282565f770a72dc6cff
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-29 09:02:48 +0100

    05 vytvorene testove metody

--- 05/Uloha1/KalkulackaTest.java
+++ 05/Uloha1/KalkulackaTest.java
@@ -20,5 +20,21 @@ public class KalkulackaTest {
     public void setUp() {
         
     }
+    
+    @Test
+    public void spocitaj5a3() {
+        Kalkulacka kalkulacka = new Kalkulacka();
+        kalkulacka.zadajCislo(5);
+        kalkulacka.pripocitaj(3);
+        Assert.assertEquals(8, kalkulacka.getVysledok(), 0.000001);
+    }
+    
+    @Test
+    public void spocitaj5a0() {
+        Kalkulacka kalkulacka = new Kalkulacka();
+        kalkulacka.zadajCislo(5);
+        kalkulacka.pripocitaj(0);
+        Assert.assertEquals(5, kalkulacka.getVysledok(), 0.000001);
+    }
 }
 
