c1a021f9a7592be06914c2c90f5f33b827f1ed05
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-05 10:20:50 +0100

    06 implementacia hraca a pocitaca

---
+++ 06/RPSLS/Hrac.java
@@ -0,0 +1,18 @@
+public class Hrac {
+    private Ruka ruka;
+    private Manazer manazer;
+    
+    public Hrac() {
+        this.ruka = new Ruka(100, 100);
+        this.manazer = new Manazer();
+        this.manazer.spravujObjekt(this);
+    }
+    
+    public void posunDole() {
+        this.ruka.predchadzajuca();
+    }
+    
+    public void posunHore() {
+        this.ruka.nasledujuca();
+    }
+}
---
+++ 06/RPSLS/Pocitac.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Pocitac {
+    private Ruka ruka;
+    private Manazer manazer;
+    private Random nahodneCisla;
+    
+    public Pocitac() {
+        this.ruka = new Ruka(200, 100);
+        this.manazer = new Manazer();
+        this.manazer.spravujObjekt(this);
+        this.nahodneCisla = new Random();
+    }
+    
+    public void tik() {
+        int gesto = this.nahodneCisla.nextInt(5);
+        this.ruka.setGesto(gesto);
+    }
+}
