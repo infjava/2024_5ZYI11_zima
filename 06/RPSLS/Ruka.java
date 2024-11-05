21e708f9e20c4fdb2b814ced694b8661b9322f20
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-05 09:43:51 +0100

    06 zobrazenie obrazku ruky

--- 06/RPSLS/Ruka.java
+++ 06/RPSLS/Ruka.java
@@ -1,6 +1,10 @@
 public class Ruka {
+    private Obrazok obrazokRuky;
+    
     public Ruka(int x, int y) {
-        
+        this.obrazokRuky = new Obrazok("pics/rock.png");
+        this.obrazokRuky.zmenPolohu(x, y);
+        this.obrazokRuky.zobraz();
     }
     
     public void setGesto(int cislo) {
