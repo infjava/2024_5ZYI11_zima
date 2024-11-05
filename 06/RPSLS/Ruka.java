301de0cbb398868c17546bc995745b54749a8e19
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-05 09:44:05 +0100

    06 zmena obrazku ruky

--- 06/RPSLS/Ruka.java
+++ 06/RPSLS/Ruka.java
@@ -8,7 +8,23 @@ public class Ruka {
     }
     
     public void setGesto(int cislo) {
-        
+        switch (cislo) {
+            case 0:
+                this.obrazokRuky.zmenObrazok("pics/rock.png");
+                break;
+            case 1:
+                this.obrazokRuky.zmenObrazok("pics/paper.png");
+                break;
+            case 2:
+                this.obrazokRuky.zmenObrazok("pics/scissors.png");
+                break;
+            case 3:
+                this.obrazokRuky.zmenObrazok("pics/spock.png");
+                break;
+            case 4:
+                this.obrazokRuky.zmenObrazok("pics/lizard.png");
+                break;
+        }
     }
     
     public void predchadzajuca() {
