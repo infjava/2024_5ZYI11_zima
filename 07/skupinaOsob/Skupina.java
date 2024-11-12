7d3efdf71b4a758f0c04e2e168eaf12a38e253f3
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-12 09:05:58 +0100

    07 divoke prechadzanie kontajnera pomocou do-while

--- 07/skupinaOsob/Skupina.java
+++ 07/skupinaOsob/Skupina.java
@@ -26,8 +26,13 @@ public class Skupina {
     }
         
     public void vypisClenovSkupiny() {
-        // Doplnit!
-        // Ukazat vsetky 3 cykly - foreach, while s lokalnou premennou aj for (pristup k prvkom cez get(i)
+        if (!this.osoby.isEmpty()) {
+            int index = 0;
+            do {
+                this.osoby.get(index).vypis();
+                index++;
+            } while (index < this.osoby.size());
+        }
     }
     
     public void odstranPodlaPoradia(int poradie) {
