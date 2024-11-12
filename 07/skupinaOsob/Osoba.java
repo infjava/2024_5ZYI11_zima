ddcf4eeaa2b525e85baf1d4f4d55ee85caec2ecd
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-12 10:31:55 +0100

    07 hodnotiaci list studenta

--- 07/skupinaOsob/Osoba.java
+++ 07/skupinaOsob/Osoba.java
@@ -22,6 +22,38 @@ public class Osoba {
     public void pridajZnamku(double znamka) {
         this.znamky.add(znamka);
     }
+    
+    public void vypisHodnotiaciList() {
+        if (this.znamky.isEmpty()) {
+            System.out.format("Student %s %s nema zadane ziadne znamky.%n", this.meno, this.priezvisko);
+            return;
+        }
+        
+        System.out.format("Znamky studenta %s %s:%n", this.meno, this.priezvisko);
+        
+        double najlepsiaZnamka = 4;
+        double najhorsiaZnamka = 1;
+        double sucetZnamok = 0;
+        
+        for (double znamka : this.znamky) {
+            System.out.println(znamka);
+            
+            if (znamka < najlepsiaZnamka) {
+                najlepsiaZnamka = znamka;
+            }
+            
+            if (znamka > najhorsiaZnamka) {
+                najhorsiaZnamka = znamka;
+            }
+            
+            sucetZnamok += znamka;
+        }
+        
+        System.out.format("Najlepsia znamka: %.1f%n", najlepsiaZnamka);
+        System.out.format("Najhorsia znamka: %.1f%n", najhorsiaZnamka);
+        System.out.format("Priemerna znamka: %.1f%n", sucetZnamok / this.znamky.size());
+        System.out.format("Pocet znamok: %d%n", this.znamky.size());
+    }
 
     public void vypis() {
         System.out.println(this.meno + " " + this.priezvisko);
