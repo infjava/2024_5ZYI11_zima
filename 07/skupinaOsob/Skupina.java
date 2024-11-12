29f6194d2aaa8a164b572f7c8cda57f8862c70fa
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-12 08:58:12 +0100

    07 ArrayList v skupine osob

--- 07/skupinaOsob/Skupina.java
+++ 07/skupinaOsob/Skupina.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
+
 public class Skupina {
-    // Tu by som chcel ZOZNAM osob (napriklad Vasu studijnu skupinu)
-    // Vopred ale neviem, kolko Vas je.
+    private ArrayList<Osoba> osoby;
         
     public Skupina() {
-        // Tu by sa mal ten zoznam vytvorit. 
-        // Doplnit!
+        this.osoby = new ArrayList<Osoba>();
     }
     
     public void pridajObjektOsoba(Osoba novaOsoba) {
